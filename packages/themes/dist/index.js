2c8ee3f157714f2d9085e9ab432873abff8f7453
Alexandr Kozhevnikov <aedkozhevnikov@sberbank.ru>
2022-03-28 15:44:09 +0300

    themes

--- packages/themes/dist/index.js
+++ packages/themes/dist/index.js
@@ -3,6 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.md = void 0;
-const material_design_1 = __importDefault(require("./material-design"));
-exports.md = material_design_1.default;
+exports.bootstrap = exports.bulma = exports.md = void 0;
+var material_design_1 = require("./material-design");
+Object.defineProperty(exports, "md", { enumerable: true, get: function () { return __importDefault(material_design_1).default; } });
+var bulma_1 = require("./bulma");
+Object.defineProperty(exports, "bulma", { enumerable: true, get: function () { return __importDefault(bulma_1).default; } });
+var bootstrap_1 = require("./bootstrap");
+Object.defineProperty(exports, "bootstrap", { enumerable: true, get: function () { return __importDefault(bootstrap_1).default; } });
--- packages/themes/index.ts
+++ packages/themes/index.ts
@@ -1,3 +1,3 @@
-import md from "./material-design"
-
-export { md }
+export { default as md } from "./material-design"
+export { default as bulma } from "./bulma"
+export { default as bootstrap } from "./bootstrap"
