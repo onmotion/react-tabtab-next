5f57c95818425a2866f7f723ac17155b614924e2
Alexandr Kozhevnikov <aedkozhevnikov@sberbank.ru>
2022-04-14 18:42:11 +0300

    demo markup

--- demo/src/data.tsx
+++ demo/src/data.tsx
@@ -10,9 +10,7 @@ export const makeData = (number: number, titlePrefix = 'Tab', useTitleCounter = 
                     <b>Content {i}</b>
                     <p>
                         Neu instabilis illas montibus glomeravit Montibus sed ignotas. Spisso legebantur phoebe montes.
-                        Dedit permisit. Glomeravit oppida formaeque ab consistere aera quam totidemque ipsa? Aera
-                        extendi persidaque tanta membra spisso illi nam solidumque. Habitandae triones abscidit traxit
-                        ita fossae aequalis utque.
+                        Dedit permisit. Glomeravit oppida formaeque ab consistere aera quam totidemque ipsa?
                     </p>
                 </div>
             ),
